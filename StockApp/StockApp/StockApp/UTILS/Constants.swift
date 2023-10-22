68f8c070ac40607e2bc6920ea250e24253e1c621
carrie6230 <carrie1999@yeah.net>
2023-10-21 17:08:06 -0700

    COMMIT THE FILES

---
+++ StockApp/StockApp/StockApp/UTILS/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StockApp
+//
+//  Created by Yanting Chen on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "d5db2ee5d3ed2f26db29c6c7a734de9e"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
