9e0cdc6f385f7d6e96d066035bed6ac607871401
Yanting Chen <114270560+carrie6230@users.noreply.github.com>
2023-12-09 20:44:17 -0800

    Add files via upload
    created an app which gets the values from a different VC, once the name and phone number is added in the Second VC use protocol and delegate to get the values the Name and Phone number put it in the table vc for the first VC

---
+++ NamePhoneNumberProtocolDelegate/NamePhoneNumberProtocolDelegate/SendNamePhoneNumberDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SendNamePhoneNumberDelegate.swift
+//  NamePhoneNumberProtocolDelegate
+//
+//  Created by Yanting Chen on 12/9/23.
+//
+
+import Foundation
+
+protocol SendNamePhoneNumberDelegate{
+    func sendNamePhoneNumber(name: String, phoneNumber: String)
+}
